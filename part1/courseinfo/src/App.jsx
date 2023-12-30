ab5619de4ff3da75ec8a29e7ea1c7f9f4fa3624d
Julien ARNOULD <ju.arnould@gmail.com>
2023-12-30 08:58:10 +0100

    Exercice 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+
+const Header = ({course}) => {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  )   
+}
+
+const Content = ({part, exercice}) => {
+  return (
+    <>
+      <p>{part} {exercice}</p>
+    </>
+  )   
+}
+
+const Total = ({total}) => {
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )   
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercice={exercises1} />
+      <Content part={part2} exercice={exercises2} />
+      <Content part={part3} exercice={exercises3} />
+      <Total total= {exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
