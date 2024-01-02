bc4e6e20b8760b4ab49c6bdaa3f4c3cdcc3f13e6
Julien ARNOULD <ju.arnould@gmail.com>
2024-01-02 18:55:01 +0100

    Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,20 @@ import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick} >{text}</button>
 const Display = ({text, number}) => <p>{text} {number}</p>
-
+const Statistics = ({good, neutral, bad}) => {
+  const total=good+neutral+bad
+  return (
+    <div>
+    <h1> Statistics</h1>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {total}</p>
+    <p>average {(good*1+bad*-1)/total}</p>
+    <p>positive {String(good/total*100)+" %"}</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -10,22 +23,14 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total=good+neutral+bad;
-
-
   return (
     <div>
       <h1>give feedback</h1>
       <Button onClick={() => setGood(good+1)} text="good"/>
       <Button onClick={() => setNeutral(neutral+1)} text="neutral"/>
       <Button onClick={() => setBad(bad+1)} text="bad"/>
-      <h1> Statistics</h1>
-      <Display text="good" number={good} />
-      <Display text="neutral" number={neutral} />
-      <Display text="bad" number={bad} />
-      <Display text="all" number={total} />
-      <Display text="average" number={(good*1+bad*-1)/total} />
-      <Display text="positive" number={String(good/total*100)+" %"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+     
     </div>
   )
 }
