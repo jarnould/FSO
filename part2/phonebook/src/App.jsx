826ff97499a47c53317664db5ede91ab8a2ab308
Julien ARNOULD <ju.arnould@gmail.com>
2024-01-06 18:42:09 +0100

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({onChange}) => 
   <div>
-    filter shown with: <input onChange={onChange}/>
+    filter shown with: <input name="filter" onChange={onChange}/>
   </div> 
 
 const PersonForm = ({onSubmit, onNameChange, onNumberChange}) =>  
   <form onSubmit={onSubmit}>
     <div>
-      name: <input onChange={onNameChange}/>
+      name: <input name="name" onChange={onNameChange}/>
     </div>
     <div>
-      number: <input onChange={onNumberChange}/>
+      number: <input name="number" onChange={onNumberChange}/>
     </div>
     <div>
       <button type="submit">add</button>
@@ -31,15 +32,16 @@ const Person = ({name, number}) =>
   <p>{name} {number}</p>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const handleNameChange = (ev) => setNewName(ev.target.value)
   const handleNumberChange = (ev) => setNewNumber(ev.target.value)
