6ff6b1a91c40cc7fe27682d898272e33e68ff840
Julien ARNOULD <ju.arnould@gmail.com>
2024-01-13 18:50:46 +0100

    Exercise 2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import personService from './services/persons'
-import persons from './services/persons'
+
+const Notification = ({notification}) => {
+  if (notification.type === null) {
+    return null
+  }
+
+  return (
+    <div className={`notification ${notification.type}`}>
+      {notification.message}
+    </div>
+  )
+}
 
 const Filter = ({onChange}) => 
   <div>
@@ -21,20 +31,20 @@ const PersonForm = ({onSubmit, onNameChange, onNumberChange}) =>
     </div>
   </form>
 
-const Persons = ({persons, filter, onClickDelete}) => {
+const Persons = ({persons, filter, deletePerson}) => {
   const personsFiltered = persons.filter(person => person.name.toLowerCase().indexOf(filter.toLowerCase())!==-1)
   return (
     personsFiltered.map(person => 
-      <Person key={person.id} person={person} onClickDelete={onClickDelete}/>
+      <Person key={person.id} person={person} deletePerson={deletePerson}/>
     )
   )
 }  
 
-const Person = ({person, onClickDelete }) => {
+const Person = ({person, deletePerson }) => {
   return (
     <p>
       {person.name} {person.number} {''}
-      <button  onClick={() => onClickDelete(person.id, person.name)}>delete</button>
+      <button  onClick={() => deletePerson(person.id, person.name)}>delete</button>
     </p>
   )
 }
@@ -44,6 +54,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState({type: null}) 
 
   useEffect(() => {
     personService
@@ -54,46 +65,56 @@ const App = () => {
   const handleNameChange = (ev) => setNewName(ev.target.value)
   const handleNumberChange = (ev) => setNewNumber(ev.target.value)
   const handleFilterChange = (ev) => setFilter((ev.target.value))
-  const handleClickDelete = (id, name) => {
+   
+  const addPerson = (el) => {
+    el.preventDefault()
+    if (newName  && newNumber) {
+      const pers=persons.find(el => el.name === newName)   
+      
+      if (pers === undefined) 
+        personService
+        .createPerson({name: newName, number: newNumber})
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNotification({type: 'info', message:`${newName} added`})
+          setTimeout(() => setNotification({type: null}), 5000)
+        })
+      
+      else if(confirm(`${pers.name} is already added to phonebook, replace the old number with a new one ?`))
+        personService
+          .updatePerson(pers.id, {...pers, number: newNumber})
+          .then(response => {
+            setPersons(persons.map(person => person.id !== response.data.id ? person : response.data))
+            setNotification({type: 'info', message:`${pers.name} updated`})
+            setTimeout(() => setNotification({type: null}), 5000) 
+          })
+          .catch(error => {
+            setNotification({type: 'error', message:`${pers.name} has already been removed from the server`})
+            setPersons(persons.filter(person => person.id !== pers.id))
+            setTimeout(() => setNotification({type: null}), 5000)
+          })  
+    }
+
+    else alert(`name and phone must be filled in`)
+  } 
+
+  const deletePerson = (id, name) => {
     if (confirm(`Delete ${name} ?`)) 
       personService
         .deletePerson(id)
-        .then (
-          setPersons(
-            persons.filter((person)=> person.id !== id )
-          )
-        )
+        .then(setPersons(persons.filter(person => person.id !== id )))
+        .catch(error => console.log(`${name} has already been removed from the server`))
   }
-     
-    const addPerson = (el) => {
-    el.preventDefault()
-    if (newName  && newNumber) {
-      const newPerson=persons.find(el => el.name === newName) 
-      if (newPerson === undefined) 
-        personService
-        .createPerson({name: newName, number: newNumber})
-        .then(response => setPersons(persons.concat(response.data)))
-         
-  //      setPersons(persons.concat({name: newName, number: newNumber}))
-      else if(confirm(`${newName} is already added to phonebook, replace the old number with a new one ?`))
-        personService
-          .updatePerson(newPerson.id, {...newPerson, number: newNumber})
-          .then(response =>
-            setPersons(persons.map(person => person.id !== response.data.id ? person : response.data))    
-          )  
-    }
-    else alert(`name and phone must be filled in`)
-    } 
   
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification} />
       <Filter onChange={handleFilterChange}/>
       <h3>add a new</h3>
       <PersonForm onSubmit={addPerson} onNameChange={handleNameChange} onNumberChange={handleNumberChange} /> 
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} onClickDelete={handleClickDelete} /> 
+      <Persons persons={persons} filter={filter} deletePerson={deletePerson} /> 
     </div>
   )
 }
