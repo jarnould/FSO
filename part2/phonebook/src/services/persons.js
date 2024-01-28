e464ec818024ee94c5797651dcd3ce2c87d7d50d
Julien ARNOULD <ju.arnould@gmail.com>
2024-01-28 16:24:17 +0100

    Exercise 3.11

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getPersons = () => axios.get(baseUrl)
 const createPerson = (newPerson) => axios.post(baseUrl, newPerson)
