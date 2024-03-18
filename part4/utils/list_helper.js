0e9f5f40d96916e932eef9690c16c441fe52ef65
Julien ARNOULD <ju.arnould@gmail.com>
2024-03-18 17:31:34 +0100

    Exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,10 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => blogs.reduce((sumOfLikes, blog) => sumOfLikes + blog.likes, 0)
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
